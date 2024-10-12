76b42168bd702c6df77d07c6fa930886b9595e36
mohamedmostafa <mohamed.mostafa@leannode.com>
2024-10-12 23:31:36 +0300

    NF-enable api version

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import {RedisIoAdapter} from "./config/redisIoAdapter.config";
+import {VersioningType} from "@nestjs/common";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableVersioning({
+    type: VersioningType.URI,
+  });
 
   //add redis adapter
   const redisIoAdapter = new RedisIoAdapter(app);
